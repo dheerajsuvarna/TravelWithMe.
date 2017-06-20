3aaeb8f630a38ec776777a6efec6e35e4182ed36
Bakri <bakri.bitar@tum.de>
2017-06-20 19:57:48 +0200

    updated user profile page

--- src/app/view-profile/view-profile.component.ts
+++ src/app/view-profile/view-profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertService, UserService } from '../services/index';
+import { User } from '../models/usermodel';
+
 
 @Component({
   selector: 'app-view-profile',
@@ -7,8 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewProfileComponent implements OnInit {
 
-  constructor() { }
+  getAge(dateString) {
+    var today = new Date();
+    var birthDate = new Date(dateString);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
+    }
+    return age;
+  }
+   currentUser: User;
+  constructor(private userService: UserService) {
 
+    this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    var temp  = localStorage.getItem('currentUser');
+    var json = JSON.parse(temp);
+    this.currentUser = json.user;
+    this.currentUser.gender= json.gender;
+    this.currentUser.description = "We should bring this value from DB or store it";
+    this.currentUser.age= new Number(this.getAge(this.currentUser.birthdate));
+    // this.currentUser.age= new Number(this.getAge(this.currentUser.birthdate));
+  }
   ngOnInit() {
   }
 
