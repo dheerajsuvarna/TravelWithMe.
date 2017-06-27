73287d9a1ca8d9f33aceb88b5188e771bcbce4bc
Bakri <bakri.bitar@tum.de>
2017-06-27 23:09:49 +0200

    fixed style and added owner name

--- src/app/my-trips/my-trips.component.ts
+++ src/app/my-trips/my-trips.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Trip} from '../models/tripmodel';
 import {Interest} from '../../models/Enums/Interest';
+import {User} from "../models/usermodel";
 
 @Component({
   selector: 'app-my-trips',
@@ -10,14 +11,17 @@ import {Interest} from '../../models/Enums/Interest';
 export class MyTripsComponent implements OnInit {
   public trip: Trip =new Trip();
   public trip2: Trip =new Trip();
-  public trips: Trip[] = [this.trip,this.trip2];
+  user: User = new User();
 
+
+  public trips: Trip[] = [this.trip,this.trip2];
 
   constructor() {
 
   }
-
   ngOnInit() {
+    this.user.firstname = "Bakri";
+    this.user.lastname="Bitar";
     this.trip.interests= [Interest.Beach];
     this.trip.tripName = "To the Beach";
     this.trip.budget = 50;
@@ -25,6 +29,7 @@ export class MyTripsComponent implements OnInit {
     this.trip.destination = "Miami";
     this.trip.source = "Munich";
     this.trip.date= "22.07.2017";
+    this.trip.user = this.user;
 
 
     this.trip2.interests= [Interest.Arts];
@@ -34,6 +39,8 @@ export class MyTripsComponent implements OnInit {
     this.trip2.destination = "Deutsches Museum";
     this.trip2.source = "Munich";
     this.trip2.date= "30.07.2017";
+    this.trip2.user = this.user;
+
 
   }
 
