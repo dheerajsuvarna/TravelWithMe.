164fee58e8e1f82be6aa629159a7badb63d59e73
Bakri <bakri.bitar@tum.de>
2017-06-28 00:34:01 +0200

    fixed style, added more interests

--- src/models/Enums/Interest.ts
+++ src/models/Enums/Interest.ts
@@ -2,8 +2,15 @@ export enum Interest {
     Arts =1,
     Beach,
     Books ,
+    Camping,
+      Dancing,
+      Fishing,
+      Jogging,
     Movies,
     Music,
   Mountains,
+      Party,
+  Swimming,
+
 
 }
