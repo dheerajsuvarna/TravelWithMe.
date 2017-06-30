4878c5e277993dab1e778f7b1e30fe1dd9e5687a
Bakri <bakri.bitar@tum.de>
2017-06-30 13:28:24 +0200

    deleted auto load all users created new register method to create temp user with verification email done. The verification process is not done yet

---
+++ src/models/TempUserModel.js
@@ -0,0 +1,91 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt');
+
+const saltRounds = 10
+
+var TempUserSchema = new Schema({
+  password: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique:true,
+  },
+  age: {
+    type: Number,
+    required: false
+  },
+  birthdate:{
+    type: String,
+    required: false
+
+  },
+  gender: {
+    type: String,
+    required: false
+
+  },
+  nationality: {
+    type: String,
+    required: false
+
+  },
+  firstname: {
+    type: String,
+    required: false
+
+    // required: true
+  },
+  lastname: {
+    type: String,
+    required: false
+
+    // required: true
+  },
+  description: {
+    type: String,
+    required: false
+
+    // required: true
+  },
+
+  GENERATED_VERIFYING_URL: {
+    type: String,
+    required: false
+
+    // required: true
+  },
+
+
+
+});
+
+TempUserSchema.pre('save', function (next) {
+  var user = this;
+  if (this.isModified('password') || this.isNew) {
+    bcrypt.hash(user.password, saltRounds, function (err, hash) {
+      if (err) {
+        return next(err);
+      }
+      user.password = hash;
+      next();
+    });
+  } else {
+    return next();
+  }
+});
+TempUserSchema.methods.comparePassword = function (passw, cb) {
+  bcrypt.compare(passw, this.password, function (err, isMatch) {
+    if (err) {
+      return cb(err);
+    }
+    cb(null, isMatch);
+  });
+}
+
+
+
+module.exports = mongoose.model('TempUser', TempUserSchema);
