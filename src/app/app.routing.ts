3501c9bc959c01cdcb3c4297acbb18e74af69f1c
Dheeraj Kumar <ga74wiy@mytum.de>
2017-07-06 20:01:18 +0530

    .

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,5 +1,5 @@
 import { Routes, RouterModule } from '@angular/router';
-
+import { SearchTripComponent } from './search-trip/search-trip.component';
 import { AuthGuard } from './guards/auth.guard';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
@@ -8,17 +8,16 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { profileManagementComponent } from './profile-management/profile-management.component';
 import { ViewProfileComponent } from './view-profile/view-profile.component';
 import { MyTripsComponent} from './my-trips/my-trips.component';
-import { SearchTripComponent } from './search-trip/search-trip.component';
+
 
 const appRoutes: Routes = [
 
    { path: '', component: HomeComponent, canActivate: [AuthGuard] },
   { path: 'profileEdit', component: profileManagementComponent, canActivate: [AuthGuard] },
   { path: 'viewProfile', component: ViewProfileComponent, canActivate: [AuthGuard] },
-
-    { path: 'login', component: LoginComponent },
-    { path: 'register', component: RegisterComponent },
-    { path: 'landingPage', component: LandingPageComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'landingPage', component: LandingPageComponent },
   { path: 'mytrips', component: MyTripsComponent, canActivate: [AuthGuard] },
   { path: 'searchTrip', component: SearchTripComponent, canActivate: [AuthGuard] },
 
