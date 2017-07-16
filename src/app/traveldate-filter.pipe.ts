d43989db56aec41ba5e96ced07d3f3b87e4c63ef
Dheeraj Kumar <ga74wiy@mytum.de>
2017-07-16 13:42:22 +0200

    .

--- src/app/traveldate-filter.pipe.ts
+++ src/app/traveldate-filter.pipe.ts
@@ -5,11 +5,14 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TraveldateFilterPipe implements PipeTransform {
 
-  transform(value: any, date: any, isFlexible: any): any {
+  transform(value: any, date: Date, isFlexible: any): any {
     if (date === undefined || date === null) {
       return value;
     }
     if(isFlexible ){
+      console.log("Flexible Date");
+      console.log(date);
+      return value.filter((item) => item.startDate.includes(date));
     }
     return value.filter((item) => item.startDate.includes(date));
   }
